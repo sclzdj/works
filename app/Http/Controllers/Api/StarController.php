acf875d3d574630b581a7a14ead0a6b9522430c6
torvalds8462 <torvalds8462@163.com>
2019-12-26 23:39:55 +0800

    修改完成个人海报

--- app/Http/Controllers/Api/StarController.php
+++ app/Http/Controllers/Api/StarController.php
@@ -350,12 +350,7 @@ class StarController extends BaseController
         $data['url2'] = $this->getPersonStyle2($xacodeImgage, $photographer, $photographer_city, $photographer_rank, $text);
         $data['url3'] = $this->getPersonStyle3($xacodeImgage, $photographer, $photographer_city, $photographer_rank, $text);
 
-        echo $data['url3'] . PHP_EOL;
-
-
-        die();
-
-
+        return $data;
     }
 
 
@@ -430,18 +425,18 @@ class StarController extends BaseController
         $handle[] = $bg;
         $handle[] = "|watermark/3/image/" . base64_urlSafeEncode("https://file.zuopin.cloud/FqRtRSleuVUJEN61BSRXvszMmzTH") . "/gravity/South/dx/0/dy/0/";
         $handle[] = "image/" . $xacodeImgage . "/gravity/SouthEast/dx/100/dy/325/";
-        $handle[] = "text/" . \Qiniu\base64_urlSafeEncode("微信扫一扫 看全部作品") . "/fontsize/720/fill/" . base64_urlSafeEncode("#F7F7F7") . "/font/" . base64_urlSafeEncode("微软雅黑") . "/gravity/SouthWest/dx/141/dy/338/";
-        $handle[] = "text/" . \Qiniu\base64_urlSafeEncode($photographer->name) . "/fontsize/1300/fill/" . base64_urlSafeEncode("#323232") . "/fontstyle/" . base64_urlSafeEncode("Bold") . "/font/" . base64_urlSafeEncode("Microsoft YaHei") . "/gravity/SouthWest/dx/98/dy/529/";
-        $handle[] = "text/" . \Qiniu\base64_urlSafeEncode($photographer_city . ' · ' . $photographer_rank . '摄影师') . "/fontsize/720/fill/" . base64_urlSafeEncode("#646464") . "/font/" . base64_urlSafeEncode("微软雅黑") . "/gravity/SouthWest/dx/99/dy/457/";
+        $handle[] = "text/" . \Qiniu\base64_urlSafeEncode("微信扫一扫 看全部作品") . "/fontsize/720/fill/" . base64_urlSafeEncode("#F7F7F7") . "/font/" . base64_urlSafeEncode("微软雅黑") . "/gravity/SouthWest/dx/140/dy/333/";
+        $handle[] = "text/" . \Qiniu\base64_urlSafeEncode($photographer->name) . "/fontsize/1300/fill/" . base64_urlSafeEncode("#323232") . "/fontstyle/" . base64_urlSafeEncode("Bold") . "/font/" . base64_urlSafeEncode("Microsoft YaHei") . "/gravity/SouthWest/dx/100/dy/520/";
+        $handle[] = "text/" . \Qiniu\base64_urlSafeEncode($photographer_city . ' · ' . $photographer_rank . '摄影师') . "/fontsize/720/fill/" . base64_urlSafeEncode("#646464") . "/font/" . base64_urlSafeEncode("微软雅黑") . "/gravity/SouthWest/dx/100/dy/450/";
 
         // 最下面那行
         $footerFont = mb_substr(implode(' · ', $text), 0, 34);
         mb_strlen(implode(' · ', $text)) > 34 ? $footerFont .= '…' : "";
 
-        $handle[] = "text/" . \Qiniu\base64_urlSafeEncode($footerFont) . "/fontsize/720/fill/" . base64_urlSafeEncode("#969696") . "/font/" . base64_urlSafeEncode("微软雅黑") . "/gravity/SouthWest/dx/99/dy/98/";
+        $handle[] = "text/" . \Qiniu\base64_urlSafeEncode($footerFont) . "/fontsize/720/fill/" . base64_urlSafeEncode("#969696") . "/font/" . base64_urlSafeEncode("微软雅黑") . "/gravity/SouthWest/dx/100/dy/90/";
         $endKey = count($text);
 
-        $indexPos = 190;
+        $indexPos = 180;
         foreach ($text as $key => $item) {
             $handle[] = "text/" . \Qiniu\base64_urlSafeEncode($item) .
                 "/fontsize/2000/fill/" . base64_urlSafeEncode("#FFFFFF") .
@@ -454,13 +449,13 @@ class StarController extends BaseController
             "/fontsize/2000/fill/" . base64_urlSafeEncode("#FFFFFF") .
             "/fontstyle/" . base64_urlSafeEncode("Bold") .
             "/font/" . base64_urlSafeEncode("Microsoft YaHei") .
-            "/gravity/NorthWest/dx/100/dy/" . ($indexPos + ($endKey  * 150)) . "/";
+            "/gravity/NorthWest/dx/100/dy/" . ($indexPos + ($endKey  * 160)) . "/";
 
         $handle[] = "text/" . \Qiniu\base64_urlSafeEncode("都是我拍的") .
             "/fontsize/2000/fill/" . base64_urlSafeEncode("#FFFFFF") .
             "/fontstyle/" . base64_urlSafeEncode("Bold") .
             "/font/" . base64_urlSafeEncode("Microsoft YaHei") .
-            "/gravity/SouthWest/dx/100/dy/946/";
+            "/gravity/West/dx/100/dy/80/";
 
         $handle[] = "|imageslim";
 
