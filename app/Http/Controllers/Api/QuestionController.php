f015d7e4a321fda65dc1199bed97b53873ee8ea3
torvalds8462 <torvalds8462@163.com>
2020-05-12 22:13:17 +0800

    merge

---
+++ app/Http/Controllers/Api/QuestionController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/21
+ * Time: 15:50
+ */
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\Auth\UserGuardController;
+
+class QuestionController extends UserGuardController
+{
+    public function index()
+    {
+
+    }
+}
