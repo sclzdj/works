d581f0d76b708a803123a667e4660f3d9dbdbd0b
jsyzchen <jsyzchenchen@gmail.com>
2020-07-21 18:57:44 +0800

    交付助手-创建作品接口

---
+++ app/Model/Index/DeliverWork.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Model\Index;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 交付助手作品模型
+ * @package App\Model\Index
+ * @author jsyzchenchen@gmail.com
+ * @date 2020/07/18
+ */
+class DeliverWork extends Model
+{
+
+}
---
+++ app/Model/Index/DeliverWorkFile.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Model\Index;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 交付助手作品文件模型
+ * @package App\Model\Index
+ * @author jsyzchenchen@gmail.com
+ * @date 2020/07/18
+ */
+class DeliverWorkFile extends Model
+{
+
+}
